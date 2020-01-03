7224c73c35dd72397320c26c301fa1ee6b08ed67
Dysticus <dysticusdev@gmail.com>
2020-01-03 13:14:36 +0100

    not working code

---
+++ src/components/button/Button.md
@@ -0,0 +1,5 @@
+Basic button:
+
+```jsx
+<Button type='primary'>Click</Button>
+```
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-	title: 'React Style Guide Example',
-	defaultExample: true,
-	webpackConfig: {
-		module: {
-			rules: [
-				{
-					test: /\.jsx?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader',
-				},
-				{
-					test: /\.css$/,
-					use: [
-						'style-loader',
-						{
-							loader: 'css-loader',
-							options: {
-								importLoaders: 1,
-								modules: true
-							}
-						},
-					],
-				},
-			],
-		},
-	},
-};
+  title: 'React Style Guide Example',
+  defaultExample: true,
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
